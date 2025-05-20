9fe322de089ae6d94f6076974561c190072f57eb
sangeeths03 <sangeeths03@github.com>
2025-05-20 11:47:39 +0530

    Add Fastlanedemoapp Xcode project for Fastlane demo

---
+++ Fastlanedemoapp/Fastlanedemoapp/FastlanedemoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastlanedemoappApp.swift
+//  Fastlanedemoapp
+//
+//  Created by sangeeths03 on 20/05/25.
+//
+
+import SwiftUI
+
+@main
+struct FastlanedemoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fastlanedemoapp/FastlanedemoappTests/FastlanedemoappTests.swift
@@ -0,0 +1,16 @@
+//
+//  FastlanedemoappTests.swift
+//  FastlanedemoappTests
+//
+//  Created by sangeeths03 on 20/05/25.
+//
+
+import Testing
+
+struct FastlanedemoappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
